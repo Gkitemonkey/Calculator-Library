bbd620a903b2c0576b978b16a25e2e7ba4c7ed58
graeme leeson <graemeleeson@hotmail.com>
2025-07-05 19:13:28 +0100

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calcaulator lilbrary containing basic math operations
 '''
 
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
