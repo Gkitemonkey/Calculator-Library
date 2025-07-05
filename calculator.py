7975d6edad67efc5f61bd63c27cf90703914a2ef
graeme leeson <graemeleeson@hotmail.com>
2025-07-05 18:59:27 +0100

    Add functions for add and minus

---
+++ calculator.py
@@ -0,0 +1,9 @@
+'''
+Calcaulator lilbrary containing basic math operations
+'''
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
